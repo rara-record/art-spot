9f431421dd10f8eaf559b362518859ca761754da
BORA <a52447879@gmail.com>
2024-11-04 17:02:39 +0900

    remove: file

--- packages/api/trpc/router.ts
+++ packages/api/trpc/router.ts
@@ -3,7 +3,6 @@ import type { inferRouterInputs, inferRouterOutputs } from "@trpc/server";
 import * as adminProcedures from "../modules/admin/procedures";
 import * as aiProcedures from "../modules/ai/procedures";
 import * as authProcedures from "../modules/auth/procedures";
-import * as newsletterProcedures from "../modules/newsletter/procedures";
 import * as teamProcedures from "../modules/team/procedures";
 import * as uploadsProcedures from "../modules/uploads/procedures";
 import { router } from "./base";
@@ -11,7 +10,6 @@ import { router } from "./base";
 export const apiRouter = router({
 	auth: router(authProcedures),
 	team: router(teamProcedures),
-	newsletter: router(newsletterProcedures),
 	ai: router(aiProcedures),
 	uploads: router(uploadsProcedures),
 	admin: router(adminProcedures),
